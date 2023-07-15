92ad339f0a68709a07aeb66c488e2507a3d795e8
gwt1cob <gwt1cob@bosch.com>
2023-07-16 01:10:09 +0530

    rest api init

--- app/main.py
+++ app/main.py
@@ -3,3 +3,29 @@ Application server.
 
 Date: July 15, 2023
 """
+from controller.NetConfController import NetConfController
+from response.response import *
+from flask import Flask,request,jsonify
+
+app = Flask(__name__)
+netconf_controller = NetConfController()
+
+
+@app.route("/connect_client",methods=["POST"])
+def connect_client():
+    code  = 400
+    client = False
+    try:
+        client_details = request.get_json()
+        client = netconf_controller.netconf_client(**client_details)
+        code = 200
+    except Exception as e:
+        code = 400
+
+    return jsonify({
+        'connected':client[0],
+        'error':client[1]
+    }),code
+
+
+app.run(host='0.0.0.0',debug=True)
