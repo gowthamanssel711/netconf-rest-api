dd8999e973015b258f7a8a3246b39454833ebc2f
gwt1cob <gwt1cob@bosch.com>
2023-07-16 12:58:45 +0530

    param validator and execute_cli

--- app/main.py
+++ app/main.py
@@ -6,7 +6,8 @@ Date: July 15, 2023
 from controller.NetConfController import NetConfController
 from flask import Flask,request,jsonify
 from logger.Log import *
-
+from utils.param_validator import validate_params
+from utils.request_params import *
 
 
 app = Flask(__name__)
@@ -16,24 +17,24 @@ server_Log = logging.getLogger("request_log")
 
 
 @app.route("/connect_client",methods=["POST"])
+@validate_params(connect_client)
 def connect_client():
-    server_Log.info(f"request to connect client {request.get_json()}")
-    code  = 400
+    server_Log.info(f"request to connect client  ---> {request.get_json()}")
     client = False
     try:
         client_details = request.get_json()
         client = netconf_controller.netconf_client(**client_details)
-        code = 200
     except Exception as e:
-        code = 400
+        server_Log.error("connect client error = {e}")
 
     return jsonify({
-        'connected':client[0],
+        'connected':str(client[0]),
         'error':client[1]
-    }),code
+    }),500 if client[1] else 200
 
 
 @app.route("/get_interface",methods=["POST"])
+@validate_params(get_interface)
 def get_interface():
     server_Log.info(f"request to get interface {request.get_json()}")
     code = 400
@@ -48,5 +49,22 @@ def get_interface():
         "interface":interface[0],
         "error":interface[1]
     }),code
+
+
+@app.route("/execute_cli",methods=["POST"])
+@validate_params(execute_cli)
+def execute_cli():
+    server_Log.info(f"execure commands in cli {request.get_json()}")
+    stdout = ''
+    stderr = ''
+    try:
+        commands = request.get_json()
+        stdout,stderr = netconf_controller.execute_cli(**commands)
+    except Exception as e:
+        stderr = str(e)
+    return jsonify({
+        'output':stdout,
+        'error':stderr
+    })
 
 app.run(host='0.0.0.0',debug=True)
